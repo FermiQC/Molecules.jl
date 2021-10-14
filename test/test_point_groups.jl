2f871b6858dd59328496a2d261e94aee231f510a
sgoodlett <stephen.goodlett25@gmail.com>
2021-10-14 13:01:36 -0400

    Fixed inefficiency with finding unique sigma_v

--- test/test_point_groups.jl
+++ test/test_point_groups.jl
@@ -13,11 +13,12 @@ RecPrism = [Atom(1, 1.0, [1.0, 1.0, 1.0]), Atom(1, 1.0, [-1.0, 1.0, 1.0]), Atom(
 RecAnti = [Atom(1, 1.0, [1.0, 1.0, 1.0]), Atom(1, 1.0, [1.0, -1.0, 1.0]), Atom(1, 1.0, [-1.0, 1.0, 1.0]), Atom(1, 1.0, [-1.0, -1.0, 1.0]), Atom(1, 1.0, [r2, 0.0, -2.0]), Atom(1, 1.0, [0.0, r2, -2.0]), Atom(1, 1.0, [-r2, 0.0, -2.0]), Atom(1, 1.0, [0.0, -r2, -2.0])]
 RecPrismO = [Atom(1, 1.0, [1.0, 1.0, 1.0]), Atom(1, 1.0, [-1.0, 1.0, 1.0]), Atom(1, 1.0, [1.0, -1.0, 1.0]), Atom(1, 1.0, [-1.0, -1.0, 1.0]), Atom(8, 15.99, [1.0, 1.0, -2.0]), Atom(8, 15.99, [-1.0, 1.0, -2.0]), Atom(8, 15.99, [1.0, -1.0, -2.0]), Atom(8, 15.99, [-1.0, -1.0, -2.0])]
 
-names = ["C1", "Ci", "Cs", "Cs_nonplanar", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2", "D3", "D2d", "D4d", "D3h", "D4h", "D5h", "D6h", "D12h", 
+names = ["C1", "Ci", "Cs", "Cs_nonplanar", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2", "D3", "D2d", "D4d", "D3h", "D4h", "D5h", "D6h", "D12h", "D100h", 
          "Cinfv", "Dinfh", "Th", "Td", "cube", "octahedron", "dodecahedron", "icosahedron"]
-pgs = ["C1", "Ci", "Cs", "Cs", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2", "D3", "D2d", "D4d", "D3h", "D4h", "D5h", "D6h", "D12h", 
+pgs = ["C1", "Ci", "Cs", "Cs", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2", "D3", "D2d", "D4d", "D3h", "D4h", "D5h", "D6h", "D12h", "D100h", 
        "Cinfv", "Dinfh", "Th", "Td", "Oh", "Oh", "Ih", "Ih"]
-
+#names = ["D100h"]
+#pgs = ["D100h"]
 @testset "Point Groups" begin
     for i = 1:size(names)[1]
         path = joinpath(@__DIR__, "sxyz/"*names[i]*".xyz")
